f2a347285852de7280ddb43624492872ec094709
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-06-19 12:06:13 +0100

    Added post deploy hook script to compress, collect and migrate.

---
+++ compress_collect_migrate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+echo "Compressing static files"
+python3 manage.py compress --force
+
+echo "Collecting static files"
+python3 manage.py collectstatic -i docs -i fonts -i *.md -i *.txt -i *.js -i *.css -i *.scss -i *.less -i *.json -i LICENSE -i *.hbs -i *.html --noinput
+
+echo "Migrating database"
+python3 manage.py migrate
