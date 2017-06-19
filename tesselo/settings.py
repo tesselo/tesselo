cc355c4d97c2412c682d83a2c81d749ad66e2052
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-06-19 18:35:22 +0100

    Added default filter backend to rest api.

--- tesselo/settings.py
+++ tesselo/settings.py
@@ -235,7 +235,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25
+    'PAGE_SIZE': 25,
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend', ),
 }
 
 # Celery settings.
