adc65e4a464113a5c23f89174b5a00f621e112df
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-07-11 11:20:42 +0100

    Added rest framework browsable api login urls.

--- tesselo/urls.py
+++ tesselo/urls.py
@@ -23,6 +23,7 @@ from tesselo.apiurls import router
 urlpatterns = [
     url(r'^docs/', include_docs_urls(title='Tesselo API Docs')),
     url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^sentinel/', include('sentinel.urls')),
     url(r'^', include('django.contrib.auth.urls')),
