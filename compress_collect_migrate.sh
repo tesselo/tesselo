d5d175617407e10768596b75622709d026068031
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-09-04 10:38:53 +0100

    Relaxed collectstatic to include all node modules files.

--- compress_collect_migrate.sh
+++ compress_collect_migrate.sh
@@ -10,12 +10,12 @@ echo "Compressing static files"
 python3 manage.py compress --force
 
 echo "Collecting static files"
-python3 manage.py collectstatic --noinput\
+ccmd = `python3 manage.py collectstatic --noinput\
+    -i tesselo\
     -i docs\
     -i fonts\
     -i *.md\
     -i *.txt\
-    -i *.js\
     -i *.scss\
     -i *.less\
     -i *.json\
