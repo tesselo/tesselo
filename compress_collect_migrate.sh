2bed97a700f12f2190f8361ed1243f545ef56ea3
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-09-04 09:10:17 +0100

    Fixed staticfiles collection patterns.

--- compress_collect_migrate.sh
+++ compress_collect_migrate.sh
@@ -10,7 +10,21 @@ echo "Compressing static files"
 python3 manage.py compress --force
 
 echo "Collecting static files"
-python3 manage.py collectstatic -i docs -i fonts -i *.md -i *.txt -i *.js -i *.css -i *.scss -i *.less -i *.json -i LICENSE -i *.hbs -i *.html --noinput
+python3 manage.py collectstatic --noinput\
+    -i docs\
+    -i fonts\
+    -i *.md\
+    -i *.txt\
+    -i *.js\
+    -i *.scss\
+    -i *.less\
+    -i *.json\
+    -i *.hbs\
+    -i *.html\
+    -i package.json\
+    -i bootswatch\
+    -i LICENSE.md\
+    -i LICENSE
 
 echo "Migrating database"
 python3 manage.py migrate
