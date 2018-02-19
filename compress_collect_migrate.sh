a1a72427ea808322176f566710e8001ec8992b89
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-02-19 16:17:32 +0000

    Made compress collect migrate script more verbose.

--- compress_collect_migrate.sh
+++ compress_collect_migrate.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -e
 
+echo Rebuilding rpm database...
 rpm --rebuilddb
+echo Installing npm...
 yum install -y --enablerepo=epel npm
+echo installing Requirejs...
 npm install -g requirejs
+echo Installing frontend deps...
 npm install --prefix frontend frontend
+echo Building frontend js...
 r.js -o frontend/js/build.js
+echo Compressing staticfiles...
 python manage.py compress --force
+echo Collecting staticfiles...
 python manage.py collectstatic --noinput \
     -i tesselo \
     -i docs \
@@ -21,5 +28,7 @@ python manage.py collectstatic --noinput \
     -i package.json \
     -i bootswatch \
     -i LICENSE.md \
-    -i LICENSE
+    -i LICENSE \
+    -i zappa_settings.json
+echo Migrating database...
 python manage.py migrate
