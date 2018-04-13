32af5dddfa91c0c3268679dd4b99e9b6394f7ea2
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-04-13 17:13:59 +0100

    Simplified list filter for naip admin.

--- apps/naip/admin.py
+++ apps/naip/admin.py
@@ -4,7 +4,7 @@ from naip.models import NAIPQuadrangle
 
 class NAIPQuadrangleModelAdmin(admin.ModelAdmin):
     search_fields = ('prefix', )
-    list_filter = ('state', 'source', 'lat', 'lon', 'subquad', )
+    list_filter = ('state', 'source', )
 
 
 admin.site.register(NAIPQuadrangle, NAIPQuadrangleModelAdmin)
