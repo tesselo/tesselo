2bbf65a231afc3da9d1c8627e31e3fe1b1052dde
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-05-03 14:55:03 +0100

    Added permissions model structure to classify app.

--- apps/classify/models.py
+++ apps/classify/models.py
@@ -1,9 +1,12 @@
 import pickle
 
+from guardian.models import GroupObjectPermissionBase, UserObjectPermissionBase
 from raster.models import RasterLayer
 
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import HStoreField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from sentinel.models import Composite, SentinelTile
 
 
@@ -88,3 +91,99 @@ class PredictedLayer(models.Model):
                 datatype=RasterLayer.CATEGORICAL,
             )
         super().save(*args, **kwargs)  # Call the "real" save() method.
+
+
+class TrainingSampleUserObjectPermission(UserObjectPermissionBase):
+    content_object = models.ForeignKey(TrainingSample, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.user, self.permission, self.content_object)
+
+
+class TrainingSampleGroupObjectPermission(GroupObjectPermissionBase):
+    content_object = models.ForeignKey(TrainingSample, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.group, self.permission, self.content_object)
+
+
+class PublicTrainingSample(models.Model):
+
+    trainingsample = models.OneToOneField(TrainingSample, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0} | {1}'.format(self.trainingsample, 'public' if self.public else 'private')
+
+
+@receiver(post_save, sender=TrainingSample, weak=False, dispatch_uid="create_trainingsample_public_object")
+def create_trainingsample_public_object(sender, instance, created, **kwargs):
+    """
+    Automatically create the public trainingsample object.
+    """
+    if created:
+        PublicTrainingSample.objects.create(trainingsample=instance)
+
+
+class ClassifierUserObjectPermission(UserObjectPermissionBase):
+    content_object = models.ForeignKey(Classifier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.user, self.permission, self.content_object)
+
+
+class ClassifierGroupObjectPermission(GroupObjectPermissionBase):
+    content_object = models.ForeignKey(Classifier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.group, self.permission, self.content_object)
+
+
+class PublicClassifier(models.Model):
+
+    classifier = models.OneToOneField(Classifier, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0} | {1}'.format(self.classifier, 'public' if self.public else 'private')
+
+
+@receiver(post_save, sender=Classifier, weak=False, dispatch_uid="create_classifier_public_object")
+def create_classifier_public_object(sender, instance, created, **kwargs):
+    """
+    Automatically create the public classifier object.
+    """
+    if created:
+        PublicClassifier.objects.create(classifier=instance)
+
+
+class PredictedLayerUserObjectPermission(UserObjectPermissionBase):
+    content_object = models.ForeignKey(PredictedLayer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.user, self.permission, self.content_object)
+
+
+class PredictedLayerGroupObjectPermission(GroupObjectPermissionBase):
+    content_object = models.ForeignKey(PredictedLayer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0} | {1} | {2}'.format(self.group, self.permission, self.content_object)
+
+
+class PublicPredictedLayer(models.Model):
+
+    predictedlayer = models.OneToOneField(PredictedLayer, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0} | {1}'.format(self.predictedlayer, 'public' if self.public else 'private')
+
+
+@receiver(post_save, sender=PredictedLayer, weak=False, dispatch_uid="create_predictedlayer_public_object")
+def create_predictedlayer_public_object(sender, instance, created, **kwargs):
+    """
+    Automatically create the public predictedlayer object.
+    """
+    if created:
+        PublicPredictedLayer.objects.create(predictedlayer=instance)
