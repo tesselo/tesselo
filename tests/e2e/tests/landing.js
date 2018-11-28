1311c6fdaac4e3d9a75433b0b105edd62abdc491
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-11-28 13:01:05 +0000

    Simplified casper e2e tests to match the teaser website.

--- tests/e2e/tests/landing.js
+++ tests/e2e/tests/landing.js
@@ -5,21 +5,7 @@ const setup = require('../setup')
 casper.test.begin('Tesselo homepage is up and running', 6, function suite(test) {
 
     casper.start(setup.get_base_url(casper), function() {
-        test.assertTitle("Tesselo", "Tesselo homepage title is the one expected");
-        test.assertExists('.btn-outline', "Main button is found");
-        test.assertTextExists('Tesselo is backed by the European Space Agency (ESA) through the ESA BIC Portugal program.');
-        test.assertExists({
-          type: 'xpath',
-          path: '//a[@href="/app/"]'
-        }, "Found launch link");
-        casper.click(x('//a[@href="/app/"]'));
-    });
-
-    casper.then(function() {
-      casper.waitForSelector('form', function(){
-        test.assertExists('form.login-form', 'Login form exists');
-        test.assertExists('button.login-button', 'Login button exists');        
-      })
+        test.assertTitle("TESSELO", "Tesselo homepage title is the one expected");
     });
 
     casper.run(function() {
