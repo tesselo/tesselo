924296fd2737d7cc95da851ad59cc0a581c4bc90
Daniel Wiesmann <daniel@tesselo.com>
2019-03-14 16:27:17 +0000

    Updated filters arguments to match django_filters update.

---
+++ apps/classify/filters.py
@@ -0,0 +1,12 @@
+from django_filters.rest_framework import FilterSet, NumberFilter
+
+from classify.models import PredictedLayer
+
+
+class PredictedLayerFilter(FilterSet):
+
+    year = NumberFilter(label='Year', field_name='composite__min_date__year')
+
+    class Meta:
+        model = PredictedLayer
+        fields = ['year', 'aggregationlayer', 'classifier', 'sentineltile', ]
