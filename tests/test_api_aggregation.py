ad6c2b6cb820c90c8ad6a0c0d774b6fd97e9f882
Daniel Wiesmann <daniel@tesselo.com>
2019-03-15 11:22:27 +0000

    Renamed aggregation test file.

--- tests/test_api_aggregation.py
+++ tests/test_api_aggregation.py
@@ -34,7 +34,6 @@ class AggregationViewTests(TestCase):
             geom='SRID=4326;MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)),((15 5, 40 10, 10 20, 5 10, 15 5)))',
         )
 
-    @skip('Test under construction.')
     def test_list_aggregation_layers(self):
         url = reverse('aggregationlayer-list')
         response = self.client.get(url)
