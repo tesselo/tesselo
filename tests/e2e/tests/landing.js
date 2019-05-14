c738ba3d0dc92b38e5b7b0453487689eea77a5c0
Daniel Wiesmann <daniel@tesselo.com>
2019-05-14 14:32:49 +0100

    Updated test title.

--- tests/e2e/tests/landing.js
+++ tests/e2e/tests/landing.js
@@ -5,7 +5,7 @@ const setup = require('../setup')
 casper.test.begin('Tesselo homepage is up and running', 1, function suite(test) {
 
     casper.start(setup.get_base_url(casper), function() {
-        test.assertTitle("TESSELO", "Tesselo homepage title is the one expected");
+        test.assertTitle("Tesselo", "Tesselo homepage title is the one expected");
     });
 
     casper.run(function() {
