e94012e0607a349c1c3221b117e0561d0df44360
Daniel Wiesmann <daniel@tesselo.com>
2019-10-25 17:07:54 +0100

    Reduced page size for report aggregation api.

--- apps/report/views.py
+++ apps/report/views.py
@@ -19,9 +19,9 @@ class ReportScheduleViewSet(PermissionsModelViewSet):
 
 
 class ReportAggregationPagination(PageNumberPagination):
-    page_size = 1000
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 5000
+    max_page_size = 50
 
 
 class ReportAggregationViewSet(ReadOnlyModelViewSet):
