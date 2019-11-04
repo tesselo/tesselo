538bcf2d25a193bd099ad509524bd029ca3df775
Daniel Wiesmann <daniel@tesselo.com>
2019-11-04 12:36:19 +0000

    Improved admin for report schedules.

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -1,10 +1,38 @@
+from raster_aggregation.models import AggregationLayer
+
+from classify.models import PredictedLayer
 from django.contrib.gis import admin
+from formulary.models import Formula
 from report.models import ReportAggregation, ReportSchedule, ReportScheduleTask
 from report.tasks import push_reports
+from sentinel.models import Composite
+
+
+class AggregationLayerInline(admin.TabularInline):
+    model = AggregationLayer.reportschedule_set.through
+    raw_id_fields = ('aggregationlayer', )
+    extra = 0
+
+
+class PredictedLayerInline(admin.TabularInline):
+    model = PredictedLayer.reportschedule_set.through
+    extra = 0
+
+
+class FormulaInline(admin.TabularInline):
+    model = Formula.reportschedule_set.through
+    extra = 0
+
+
+class CompositeInline(admin.TabularInline):
+    model = Composite.reportschedule_set.through
+    extra = 0
 
 
 class ReportScheduleAdmin(admin.ModelAdmin):
     actions = ['run_schedule', ]
+    inlines = (AggregationLayerInline, PredictedLayerInline, FormulaInline, CompositeInline, )
+    exclude = ('aggregationlayers', 'predictedlayers', 'formulas', 'composites', )
 
     def run_schedule(self, request, queryset):
         """
