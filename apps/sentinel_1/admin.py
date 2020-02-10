fadb76c710a6429cc555ccbcaf92420706ab80e7
Daniel Wiesmann <daniel@tesselo.com>
2020-02-10 17:06:34 +0000

    Improved sentinel-1 tile band admin.

--- apps/sentinel_1/admin.py
+++ apps/sentinel_1/admin.py
@@ -18,5 +18,10 @@ class Sentinel1TileAdmin(admin.OSMGeoAdmin):
         self.message_user(request, 'Triggered terrain correction for scenes {}'.format([s1tile.id for s1tile in queryset]))
 
 
+class Sentinel1TileBandAdmin(admin.ModelAdmin):
+    readonly_fields = ('tile', 'band', 'layer', )
+    raw_id_fields = ('tile', 'layer', )
+
+
 admin.site.register(Sentinel1Tile, Sentinel1TileAdmin)
-admin.site.register(Sentinel1TileBand)
+admin.site.register(Sentinel1TileBand, Sentinel1TileBandAdmin)
