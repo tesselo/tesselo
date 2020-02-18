788e7bbbc61e54d66733e3cce97bc230eab652f6
Daniel Wiesmann <daniel@tesselo.com>
2020-02-18 15:20:57 +0000

    Added api endpoint to refresh batch job status.

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from jobs.models import BatchJob
@@ -6,6 +8,7 @@ from jobs.models import BatchJob
 class BatchJobSerializer(ModelSerializer):
 
     log = SerializerMethodField()
+    description = SerializerMethodField()
 
     class Meta:
         model = BatchJob
@@ -13,6 +16,13 @@ class BatchJobSerializer(ModelSerializer):
             'id', 'job_id', 'status', 'created', 'log_stream_name',
             'description', 'log',
         )
+
+    def get_description(self, obj):
+        try:
+            desc = json.loads(obj.description)
+        except json.decoder.JSONDecodeError:
+            desc = 'Could not get description. JSON decode error.'
+        return desc
 
     def get_log(self, obj):
         return obj.get_log()
--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -1,6 +1,11 @@
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from jobs.models import BatchJob
 from jobs.serializers import BatchJobSerializer
-from raster_api.views import PermissionsModelViewSet
+from raster_api.permissions import ChangePermissionObjectPermission
+from raster_api.views import IsReadOnly, PermissionsModelViewSet
 
 
 class BatchJobViewSet(PermissionsModelViewSet):
@@ -9,3 +14,17 @@ class BatchJobViewSet(PermissionsModelViewSet):
     queryset = BatchJob.objects.all()
 
     _model = 'batchjob'
+
+    @action(detail=True, methods=['get', 'post'], permission_classes=[IsAuthenticated, IsReadOnly, ChangePermissionObjectPermission])
+    def refresh(self, request, pk):
+        """
+        Update batch job status.
+        """
+        # Get batch job object.
+        job = self.get_object()
+        # Update status
+        job.update()
+
+        return Response({
+            'success': 'Updated batch job. New status is "{}".'.format(job.status),
+        })
