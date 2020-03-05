5e7d40bbb6e0b2144678abe91f6c01e63949b96f
Daniel Wiesmann <daniel@tesselo.com>
2020-03-05 11:02:30 +0000

    Replaced base_name with basename for drf router.

--- tesselo/apiurls.py
+++ tesselo/apiurls.py
@@ -26,97 +26,97 @@ router.register(r'legendentry', LegendEntryViewSet)
 router.register(
     r'tile/(?P<layer>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     AlgebraAPIView,
-    base_name='tile'
+    basename='tile'
 )
 router.register(
     r'algebra/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>jpg|png|tif)',
     AlgebraAPIView,
-    base_name='algebra'
+    basename='algebra'
 )
 router.register(
     r'^pixel/(?P<xcoord>-?\d+(?:\.\d+)?)/(?P<ycoord>-?\d+(?:\.\d+)?)$',
     AlgebraAPIView,
-    base_name='algebra-pixel'
+    basename='algebra-pixel'
 )
 router.register(
     r'export',
     ExportAPIView,
-    base_name='export'
+    basename='export'
 )
 router.register(
     r'vtiles/(?P<aggregationlayer>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>json|pbf)',
     AggregationLayerVectorTilesViewSet,
-    base_name='vectortiles'
+    basename='vectortiles'
 )
 router.register(
     r'(?P<sentinel>sentinel)/(?P<utm_zone>[^/]+)/(?P<lat_band>[^/]+)/(?P<grid_id>[^/]+)/(?P<year>[^/]+)/(?P<month>[^/]+)/(?P<day>[^/]+)/(?P<scene_nr>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='sentinel',
+    basename='sentinel',
 )
 router.register(
     r'(?P<landsat>landsat)/(?P<collection>[^/]+)/(?P<sensor>[^/]+)/(?P<row>[^/]+)/(?P<column>[^/]+)/(?P<scene>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='landsat',
+    basename='landsat',
 )
 router.register(
     r'(?P<landsat>landsat)/(?P<sensor>[^/]+)/(?P<row>[^/]+)/(?P<column>[^/]+)/(?P<scene>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='landsat',
+    basename='landsat',
 )
 router.register(
     r'(?P<naip>naip)/(?P<state>[^/]+)/(?P<year>[^/]+)/(?P<resolution>[^/]+)/(?P<img_src>rgb|rgbir)/(?P<quadrangle>[^/]+)/(?P<scene>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='naip',
+    basename='naip',
 )
 router.register(
     r'(?P<naip>naip)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='naip_auto',
+    basename='naip_auto',
 )
 router.register(
     r'(?P<naip>naip)/(?P<year>2011|2012|2013|2014|2015|2016)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='naip_auto_year',
+    basename='naip_auto_year',
 )
 router.register(
     r'(?P<composite>composite)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)',
     LambdaView,
-    base_name='composite_layer',
+    basename='composite_layer',
 )
 
 router.register(r'aggregationlayer', AggregationLayerViewSet)
 router.register(r'aggregationarea', AggregationAreaViewSet)
 router.register(r'valuecountresult', ValueCountResultViewSet)
 
-router.register(r'composite', CompositeViewSet, base_name='composite')
-router.register(r'compositetile', CompositeTileViewSet, base_name='compositetile')
-router.register(r'compositebuild', CompositeBuildViewSet, base_name='compositebuild')
-router.register(r'sentineltileaggregationlayer', SentinelTileAggregationLayerViewSet, base_name='sentineltileaggregationlayer')
-router.register(r'sentineltile', SentinelTileViewSet, base_name='sentineltile')
+router.register(r'composite', CompositeViewSet, basename='composite')
+router.register(r'compositetile', CompositeTileViewSet, basename='compositetile')
+router.register(r'compositebuild', CompositeBuildViewSet, basename='compositebuild')
+router.register(r'sentineltileaggregationlayer', SentinelTileAggregationLayerViewSet, basename='sentineltileaggregationlayer')
+router.register(r'sentineltile', SentinelTileViewSet, basename='sentineltile')
 
-router.register(r'formula', FormulaViewSet, base_name='formula')
+router.register(r'formula', FormulaViewSet, basename='formula')
 router.register(
     r'formula/(?P<formula_id>[0-9]+)/(?P<layer_type>scene|composite)/(?P<layer_id>[0-9]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>jpg|png|tif)',
     FormulaAlgebraAPIView,
-    base_name='formula_algebra'
+    basename='formula_algebra'
 )
 
-router.register(r'wmtslayer', WMTSLayerViewSet, base_name='wmtslayer')
+router.register(r'wmtslayer', WMTSLayerViewSet, basename='wmtslayer')
 
-router.register(r'traininglayer', TrainingLayerViewSet, base_name='traininglayer')
-router.register(r'trainingsample', TrainingSampleViewSet, base_name='trainingsample')
-router.register(r'classifier', ClassifierViewSet, base_name='classifier')
-router.register(r'predictedlayer', PredictedLayerViewSet, base_name='predictedlayer')
+router.register(r'traininglayer', TrainingLayerViewSet, basename='traininglayer')
+router.register(r'trainingsample', TrainingSampleViewSet, basename='trainingsample')
+router.register(r'classifier', ClassifierViewSet, basename='classifier')
+router.register(r'predictedlayer', PredictedLayerViewSet, basename='predictedlayer')
 
-router.register(r'readonlytoken', ReadOnlyTokenViewSet, base_name='readonlytoken')
+router.register(r'readonlytoken', ReadOnlyTokenViewSet, basename='readonlytoken')
 
-router.register(r'bookmark', BookmarkViewSet, base_name='bookmark')
-router.register(r'bookmarkfolder', BookmarkFolderViewSet, base_name='bookmarkfolder')
+router.register(r'bookmark', BookmarkViewSet, basename='bookmark')
+router.register(r'bookmarkfolder', BookmarkFolderViewSet, basename='bookmarkfolder')
 
-router.register(r'reportschedule', ReportScheduleViewSet, base_name='reportschedule')
-router.register(r'reportaggregation', ReportAggregationViewSet, base_name='reportaggregation')
+router.register(r'reportschedule', ReportScheduleViewSet, basename='reportschedule')
+router.register(r'reportaggregation', ReportAggregationViewSet, basename='reportaggregation')
 
-router.register(r'batchjob', BatchJobViewSet, base_name='batchjob')
+router.register(r'batchjob', BatchJobViewSet, basename='batchjob')
 
 apiurlpatterns = [
     url(r'^token-auth/', csrf_exempt(ObtainExpiringAuthToken.as_view()), name='api-token-auth'),
