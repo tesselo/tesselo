bf00caee0f22877119d21214bd6fbf4870e6ea4f
Daniel Wiesmann <daniel@tesselo.com>
2020-04-15 21:17:07 +0100

    Changed stats percentage cover filter to use gte.

--- apps/report/filters.py
+++ apps/report/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import DateFilter, FilterSet
+from django_filters.rest_framework import DateFilter, FilterSet, NumberFilter
 
 from report.models import ReportAggregation
 
@@ -7,10 +7,11 @@ class ReportAggregationFilter(FilterSet):
 
     date_after = DateFilter(field_name='min_date', lookup_expr='gte')
     date_before = DateFilter(field_name='max_date', lookup_expr='lte')
+    min_stats_percentage_covered = NumberFilter(field_name='stats_percentage_covered', lookup_expr='gte')
 
     class Meta:
         model = ReportAggregation
         fields = (
             'formula', 'aggregationlayer', 'aggregationarea', 'predictedlayer',
-            'composite', 'date_after', 'date_before', 'stats_percentage_covered',
+            'composite', 'date_after', 'date_before', 'min_stats_percentage_covered',
         )
