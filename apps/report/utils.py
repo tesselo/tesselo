30d4b127f7a159b76cba9196acbaf0017e6eca29
Daniel Wiesmann <daniel@tesselo.com>
2020-07-02 17:34:21 +0100

    Changed new aggregation to be tile-by-tile.

--- apps/report/utils.py
+++ apps/report/utils.py
@@ -1,6 +1,5 @@
 import json
 from collections import Counter
-from contextlib import ExitStack
 from math import ceil
 
 import numpy
@@ -11,9 +10,9 @@ from raster.models import Legend
 from raster.tiles.const import WEB_MERCATOR_SRID
 from raster.valuecount import Aggregator
 from rasterio import Affine
+from rasterio.crs import CRS
 from rasterio.features import bounds, rasterize
 from rasterio.io import MemoryFile
-from rasterio.merge import merge
 from rasterio.warp import Resampling, calculate_default_transform, reproject
 
 from report.const import ALLOWED_LINEAR_UNITS
@@ -80,70 +79,47 @@ class AggregatorProjection(Aggregator):
         results = Counter({})
         self._clear_stats()
 
-        # Combine all tiles into one big array.
-        all_result_data = [tile for tile in self.tiles()]
-
-        # Merge tiles into one.
-        with ExitStack() as stack:
-            files = [stack.enter_context(dat.open()) for dat in all_result_data]
-            src_data, src_transform = merge(files)
-
-        # Close all tiles.
-        for tile in all_result_data:
-            tile.close()
-
-        # Create file from merged result.
-        src_creation_args = {
-            'driver': 'GTiff',
-            'dtype': src_data.dtype,
-            'nodata': 0,
-            'width': src_data.shape[2],
-            'height': src_data.shape[1],
-            'count': src_data.shape[0],
-            'crs': 'EPSG:{}'.format(WEB_MERCATOR_SRID),
-            'transform': src_transform,
-        }
         # Set the destination crs to the one from the input geometry.
-        dst_crs = 'EPSG:{}'.format(self.geom.srid)
+        dst_crs = CRS.from_epsg(self.geom.srid)
 
-        # Create the source and destination rasters.
-        with MemoryFile() as memfile:
-            with memfile.open(**src_creation_args) as src:
-                # Write raster data to src file.
-                src.write(src_data)
-                # Compute transformation for destination file.
-                dst_transform, width, height = calculate_default_transform(
-                    src.crs,
-                    dst_crs,
-                    src.width,
-                    src.height,
-                    *src.bounds,
-                )
-                # Create destination args.
-                dst_creation_args = src.meta.copy()
-                dst_creation_args.update({
-                    'crs': dst_crs,
-                    'transform': dst_transform,
-                    'width': width,
-                    'height': height,
-                })
-                # Creat destination file.
-                with MemoryFile() as memfile2:
-                    with memfile2.open(**dst_creation_args) as dst:
-                        # Reproject each band into the destination raster.
-                        for i in range(1, src.count + 1):
+        # Perform a sanity check on target crs.
+        if dst_crs.linear_units.lower() not in ALLOWED_LINEAR_UNITS:
+            raise RasterAggregationException('Units of dst crs need to be in meters, found {}.'.format(dst_crs.linear_units))
+
+        # Combine all tiles into one big array.
+        for creation_args, band_data in self.tiles():
+            with MemoryFile() as memfile_src:
+                with memfile_src.open(**creation_args) as src:
+                    src.write(band_data, 1)
+                    # Compute transformation for destination file.
+                    dst_transform, width, height = calculate_default_transform(
+                        src.crs,
+                        dst_crs,
+                        src.width,
+                        src.height,
+                        *src.bounds,
+                    )
+                    # Create destination args.
+                    dst_creation_args = src.meta.copy()
+                    dst_creation_args.update({
+                        'crs': dst_crs,
+                        'transform': dst_transform,
+                        'width': width,
+                        'height': height,
+                    })
+                    # Creat destination file.
+                    with MemoryFile() as memfile_dst:
+                        with memfile_dst.open(**dst_creation_args) as dst:
+                            # Reproject each band into the destination raster.
                             reproject(
-                                source=rasterio.band(src, i),
-                                destination=rasterio.band(dst, i),
+                                source=rasterio.band(src, 1),
+                                destination=rasterio.band(dst, 1),
                                 src_transform=src.transform,
                                 src_crs=src.crs,
                                 dst_transform=dst_transform,
                                 dst_crs=dst_crs,
                                 resampling=Resampling.nearest,
                             )
-                            # Perform a sanity check on target crs.
-                            if dst.crs.linear_units.lower() not in ALLOWED_LINEAR_UNITS:
-                                raise RasterAggregationException('Units of dst crs need to be in meters, found {}.'.format(dst.crs.linear_units))
                             # Compute size in m2 of the pixels in the image.
                             pixel_size_m2 = abs(dst.transform[0] * dst.transform[4])
                             # Rasterize the geometry and use the mask on all bands.
@@ -159,59 +135,58 @@ class AggregatorProjection(Aggregator):
                             # Convert the rasterized geometry into a boolean array.
                             geom_rasterized = geom_rasterized == 1
                             # Mask the destination raster using the rasterized geometry.
-                            all_result_data = [dst.read(i)[geom_rasterized] for i in range(1, dst.count + 1)]
+                            result_data = dst.read(1)[geom_rasterized].ravel()
 
-        # For the resulting array, compute the statistics.
-        for result_data in all_result_data:
-            if self.grouping == 'discrete':
-                # Compute unique counts for discrete input data
-                unique_counts = numpy.unique(result_data, return_counts=True)
-                # Add counts to results
-                values = dict(zip(unique_counts[0], unique_counts[1]))
+                            # For the resulting array, compute the statistics.
+                            if self.grouping == 'discrete':
+                                # Compute unique counts for discrete input data
+                                unique_counts = numpy.unique(result_data, return_counts=True)
+                                # Add counts to results
+                                values = dict(zip(unique_counts[0], unique_counts[1]))
 
-            elif self.grouping == 'continuous':
-                if self.memory_efficient and not self.hist_range:
-                    raise RasterAggregationException(
-                        'Secify a histogram range for memory efficient continuous aggregation.'
-                    )
+                            elif self.grouping == 'continuous':
+                                if self.memory_efficient and not self.hist_range:
+                                    raise RasterAggregationException(
+                                        'Secify a histogram range for memory efficient continuous aggregation.'
+                                    )
 
-                # Handle continuous case - compute histogram on masked data
-                counts, bins = numpy.histogram(result_data, range=self.hist_range)
+                                # Handle continuous case - compute histogram on masked data
+                                counts, bins = numpy.histogram(result_data, range=self.hist_range)
 
-                # Create dictionary with bins as keys and histogram counts as values
-                values = {}
-                for i in range(len(bins) - 1):
-                    values[(bins[i], bins[i + 1])] = counts[i]
+                                # Create dictionary with bins as keys and histogram counts as values
+                                values = {}
+                                for i in range(len(bins) - 1):
+                                    values[(bins[i], bins[i + 1])] = counts[i]
 
-            else:
-                # If input is not a legend, interpret input as legend json data
-                if not isinstance(self.grouping, Legend):
-                    self.grouping = Legend(json=self.grouping)
+                            else:
+                                # If input is not a legend, interpret input as legend json data
+                                if not isinstance(self.grouping, Legend):
+                                    self.grouping = Legend(json=self.grouping)
 
-                # Try getting a colormap from the input
-                try:
-                    colormap = self.grouping.colormap
-                except:
-                    raise RasterAggregationException(
-                        'Invalid grouping value found for valuecount.'
-                    )
+                                # Try getting a colormap from the input
+                                try:
+                                    colormap = self.grouping.colormap
+                                except:
+                                    raise RasterAggregationException(
+                                        'Invalid grouping value found for valuecount.'
+                                    )
 
-                # Use colormap to compute value counts
-                formula_parser = FormulaParser()
-                values = {}
-                for key, color in colormap.items():
-                    try:
-                        # Try to use the key as number directly
-                        selector = result_data == float(key)
-                    except ValueError:
-                        # Otherwise use it as numpy expression directly
-                        selector = formula_parser.evaluate({'x': result_data}, key)
-                    values[key] = numpy.sum(selector)
+                                # Use colormap to compute value counts
+                                formula_parser = FormulaParser()
+                                values = {}
+                                for key, color in colormap.items():
+                                    try:
+                                        # Try to use the key as number directly
+                                        selector = result_data == float(key)
+                                    except ValueError:
+                                        # Otherwise use it as numpy expression directly
+                                        selector = formula_parser.evaluate({'x': result_data}, key)
+                                    values[key] = numpy.sum(selector)
 
-            # Add counts to results.
-            results.update(Counter(values))
-            # Push statistics.
-            self._push_stats(result_data)
+                            # Add counts to results.
+                            results.update(Counter(values))
+                            # Push statistics.
+                            self._push_stats(result_data)
 
         # Transform pixel count to hectares.
         scaling_factor = 1
@@ -334,24 +309,21 @@ def gdalraster_to_rasterio(rst):
     """
     # Get list of bands.
     bands = [band for band in rst.bands]
+    if len(bands) != 1:
+        raise RasterAggregationException('For aggregation, band count needs to be exactly 1.')
+    band = bands[0]
     # Prepare affine transform to create rasterio raster.
     transform = Affine(rst.scale.x, rst.skew.x, rst.origin.x, rst.skew.y, rst.scale.y, rst.origin.y)
     # Prepare the creation args for the new rasterio raster.
     creation_args = {
         'driver': 'GTiff',
-        'dtype': bands[0].datatype(as_string=True).split('GDT_')[1].lower(),
-        'nodata': bands[0].nodata_value,
+        'dtype': band.datatype(as_string=True).split('GDT_')[1].lower(),
+        'nodata': band.nodata_value,
         'width': rst.width,
         'height': rst.height,
-        'count': len(bands),
+        'count': 1,
         'crs': 'EPSG:{}'.format(rst.srid),
         'transform': transform,
     }
     # Create memfile and copy pixel values from GDALRaster to rasterio file.
-    memfile = MemoryFile()
-    with memfile.open(**creation_args) as dst:
-        for index, band in enumerate(bands):
-            dst.write(band.data(), index + 1)
-    memfile.seek(0)
-
-    return memfile
+    return creation_args, band.data()
