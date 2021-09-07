tesselo/tesselo

commit e67358a94d844828d0dcce1f84bc71c05173d963

Further improved numpy array aggregation.

apps/sentinel/tasks.py
@@ -527,26 +527,25 @@ def process_compositetile(compositetile_id):
                         dtype = numpy.uint8
                     else:
                         dtype = numpy.uint16
-                    # Aggregate each tile in the block of 2x2.
+                    # Read each tile in the block of 2x2.
                     for idx, dat in enumerate(((0, 0), (1, 0), (0, 1), (1, 1))):
                         tile = get_raster_tile(rasterlayer_id, zoom, tilex + dat[0], tiley + dat[1])
                         if tile:
                             none_found = False
-                            agg = aggregate_tile(tile.bands[0].data(), target_dtype=dtype)
+                            agg = tile.bands[0].data()
                         else:
                             size = WEB_MERCATOR_TILESIZE // 2
                             agg = numpy.zeros((size, size), dtype=dtype)
                         result.append(agg)
-
                     # Continue if no tile could be found for this 2x2 block.
                     if none_found:
                         continue
-
-                    # Combine the aggregated version of the tiles to a new full tile at lower zoom level.
+                    # Combine the tiles to a new full tile.
                     upper = numpy.append(result[0], result[1], axis=1)
                     lower = numpy.append(result[2], result[3], axis=1)
                     result = numpy.append(upper, lower, axis=0)
-
+                    # Aggregate tile by a factor of two to match lower zoom level.
+                    result = aggregate_tile(result, target_dtype=dtype)
                     # Write pixels into a tile, using dtype and nodata specs of
                     # each satellite system.
                     if is_S1:

apps/sentinel/utils.py
@@ -22,52 +22,42 @@ from sentinel import const
 
 def aggregate_tile(tile, target_dtype=None, discrete=False):
     """
-    Aggregate a tile array to the next zoom level using movin average. Create
-    a 1-D array of half the size of the input data.
+    Use the gdal resampling algorithm to do aggregation of input numpy arrays.
 
-    Inspired by:
-    https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
+    By default, bilinear resampling will be used. If the discrete flag is set,
+    nearest neighbor resampling will be used.
     """
-    if discrete:
-        # Create arbitrary affine. These values do not matter for a simple resampling
-        # because resampling is the same anywhere in the world.
-        transform = Affine(1, 0, 0, 0, -1, 0)
-        # Prepare creation args for an in-memory tif file that we will use for resampling.
-        creation_args = {
-            'driver': 'GTiff',
-            'dtype': tile.dtype,
-            'nodata': None,
-            'width': tile.shape[1],
-            'height': tile.shape[0],
-            'count': 1,
-            'crs': f'EPSG:{WEB_MERCATOR_SRID}',
-            'transform': transform,
-        }
-        # Open a new raster in memory.
-        with MemoryFile() as memfile_src:
-            with memfile_src.open(**creation_args) as src:
-                # Write the tile data into the memory file.
-                src.write(tile, 1)
-                # Resample the tile data using the nearest neighbor mode from gdal.
-                # This is done by reading the first band and resample into smaller shape.
-                tile = src.read(
+    resampling = Resampling.nearest if discrete else Resampling.bilinear
+    # Create arbitrary affine. These values do not matter for a simple resampling
+    # because resampling is the same anywhere in the world.
+    transform = Affine(1, 0, 0, 0, -1, 0)
+    # Prepare creation args for an in-memory tif file that we will use for resampling.
+    creation_args = {
+        'driver': 'GTiff',
+        'dtype': tile.dtype,
+        'nodata': None,
+        'width': tile.shape[1],
+        'height': tile.shape[0],
+        'count': 1,
+        'crs': f'EPSG:{WEB_MERCATOR_SRID}',
+        'transform': transform,
+    }
+    # Open a new raster in memory.
+    with MemoryFile() as memfile_src:
+        with memfile_src.open(**creation_args) as src:
+            # Write the tile data into the memory file.
+            src.write(tile, 1)
+            # Resample the tile data using the nearest neighbor mode from gdal.
+            # This is done by reading the first band and resample into smaller shape.
+            tile = src.read(
+                1,
+                out_shape=(
                     1,
-                    out_shape=(
-                        1,
-                        int(creation_args['height'] / 2),
-                        int(creation_args['width'] / 2),
-                    ),
-                    resampling=Resampling.nearest,
-                )
-    else:
-        # Reshape data into subgroups to average over.
-        tile.shape = (const.AGG_TILE_SIZE, const.AGG_FACTOR, const.AGG_TILE_SIZE, const.AGG_FACTOR)
-        tile = tile.swapaxes(1, 2)
-        tile = tile.reshape(const.AGG_TILE_SIZE_SQ, const.AGG_FACTOR, const.AGG_FACTOR)
-        # Average it for continuous values.
-        tile = numpy.mean(tile, axis=(1, 2))
-        # Reshape into a squared tile.
-        tile.shape = (const.AGG_TILE_SIZE, const.AGG_TILE_SIZE)
+                    int(creation_args['height'] / 2),
+                    int(creation_args['width'] / 2),
+                ),
+                resampling=resampling,
+            )
 
     # Convert to a specific type if requested.
     if target_dtype:

tests/test_sentinel_tile_aggregators.py
@@ -7,16 +7,18 @@ from sentinel.tasks import aggregate_tile, disaggregate_tile
 class AggregatorTests(TestCase):
 
     def test_continuous_aggregator(self):
-        tile = (numpy.random.random_sample((256, 256)) * 100).astype('uint8')
+        tile = numpy.arange(512 * 512).reshape((512, 512)).astype('uint8')
         # Continuous case.
         result = aggregate_tile(tile, target_dtype='uint16')
-        self.assertEqual(numpy.mean(numpy.take(tile, [0, 1, 256, 257])).astype('uint16'), result[0, 0])
+        self.assertEqual(7, result[0, 3])
+        self.assertEqual(result.dtype, numpy.uint16)
 
     def test_discrete_aggregator(self):
-        tile = (numpy.random.random_sample((256, 256)) * 100).astype('uint8')
+        tile = numpy.arange(512 * 512).reshape((512, 512)).astype('uint8')
         # Discrete case.
-        result = aggregate_tile(tile, target_dtype='uint8', discrete=True)
-        self.assertEqual(tile.reshape(256, 256)[0, 6], result[0, 3])
+        result = aggregate_tile(tile, target_dtype='uint16', discrete=True)
+        self.assertEqual(7, result[0, 3])
+        self.assertEqual(result.dtype, numpy.uint16)
 
     def test_disaggregator(self):
         data = numpy.arange(1, 256 * 256 + 1)

commit d3401a84276364d270c72dfec009977c4b968f64

fix(pyramids): changed size for missing tiles substitute to 256
This is a regression from https://github.com/tesselo/tesselo/commit/e67358a94d844828d0dcce1f84bc71c05173d963 where the tile size for the tile-not-found case was not updated from 128 to the required size of 256.

apps/sentinel/tasks.py
@@ -534,8 +534,7 @@ def process_compositetile(compositetile_id):
                             none_found = False
                             agg = tile.bands[0].data()
                         else:
-                            size = WEB_MERCATOR_TILESIZE // 2
-                            agg = numpy.zeros((size, size), dtype=dtype)
+                            agg = numpy.zeros((WEB_MERCATOR_TILESIZE, WEB_MERCATOR_TILESIZE), dtype=dtype)
                         result.append(agg)
                     # Continue if no tile could be found for this 2x2 block.
                     if none_found: