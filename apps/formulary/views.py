c4955335115b462339128dabc063900b2a1713b4
Daniel Wiesmann <daniel@tesselo.com>
2021-09-09 15:40:04 +0100

    feat(raster): copy of get_rgb function to formulary view

--- apps/formulary/views.py
+++ apps/formulary/views.py
@@ -3,12 +3,16 @@ import os
 import uuid
 
 import numpy
+from django.contrib.gis.gdal import GDALRaster
 from django.contrib.gis.gdal.raster.const import VSI_FILESYSTEM_BASE_PATH
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from PIL import Image
+from raster.algebra.const import BAND_INDEX_SEPARATOR
 from raster.algebra.parser import RasterAlgebraParser
 from raster.const import IMG_ENHANCEMENTS, IMG_FORMATS
+from raster.tiles.const import WEB_MERCATOR_SRID, WEB_MERCATOR_TILESIZE
 from raster.utils import band_data_to_image, pixel_value_from_point
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
@@ -174,6 +178,99 @@ class FormulaAlgebraAPIView(AlgebraAPIView):
 
         # Render tile using the legend data
         img, stats = band_data_to_image(result, colormap)
+
+        # Return rendered image
+        return self.write_img_to_response(img, stats)
+
+    def get_rgb(self, data):
+        """
+        Patched directly from django-raster.
+        """
+        # Get data arrays from tiles, by band if requested.
+        for key, tile in data.items():
+
+            keysplit = key.split(BAND_INDEX_SEPARATOR)
+            variable = keysplit[0]
+
+            if len(keysplit) > 1:
+                band_index = int(keysplit[1])
+            else:
+                band_index = 0
+
+            band = tile.bands[band_index]
+            if variable == 'r':
+                red = band.data()
+                red_nodata = band.nodata_value
+                red_gdal_dtype = band.datatype()
+            elif variable == 'g':
+                green = band.data()
+                green_nodata = band.nodata_value
+            elif variable == 'b':
+                blue = band.data()
+                blue_nodata = band.nodata_value
+
+        # For tif requests, skip rgb rendering and return georeferenced tif file.
+        if self.kwargs.get('frmt') == 'tif':
+            vsi_path = os.path.join(VSI_FILESYSTEM_BASE_PATH, str(uuid.uuid4()))
+            # Construct 3 band raster, assuming all
+            ref = next(iter(data.values()))
+            result = GDALRaster({
+                'name': vsi_path,
+                'driver': 'tif',
+                'srid': WEB_MERCATOR_SRID,
+                'datatype': red_gdal_dtype,
+                'width': ref.width,
+                'height': ref.height,
+                'origin': ref.origin,
+                'scale': ref.scale,
+                'skew': ref.skew,
+                'bands': [
+                    {'data': red, 'nodata_value': red_nodata},
+                    {'data': green.astype(red.dtype), 'nodata_value': green_nodata},
+                    {'data': blue.astype(red.dtype), 'nodata_value': blue_nodata},
+                ],
+                'papsz_options': {
+                    'compress': 'deflate',
+                },
+            })
+            content_type = IMG_FORMATS['tif'][1]
+            return HttpResponse(result.vsi_buffer, content_type)
+
+        # Get scale for the image value range.
+        scale = self.get_rgb_scale()
+        if scale is not None:
+            # Clip the image minimum.
+            red[red < scale[0]] = scale[0]
+            green[green < scale[0]] = scale[0]
+            blue[blue < scale[0]] = scale[0]
+
+            # Clip the image maximum.
+            red[red > scale[1]] = scale[1]
+            green[green > scale[1]] = scale[1]
+            blue[blue > scale[1]] = scale[1]
+
+            # Scale the image.
+            red = 255 * (red - scale[0]) / scale[1]
+            green = 255 * (green - scale[0]) / scale[1]
+            blue = 255 * (blue - scale[0]) / scale[1]
+
+        if self.get_alpha():
+            mode = 'RGBA'
+            reshape = 4
+            # Create the alpha channel.
+            alpha = 255 * (red > 0) * (blue > 0) * (green > 0)
+            img_array = numpy.array((red.ravel(), green.ravel(), blue.ravel(), alpha.ravel()))
+        else:
+            mode = 'RGB'
+            reshape = 3
+            img_array = numpy.array((red.ravel(), green.ravel(), blue.ravel()))
+
+        # Reshape array into tile size.
+        img_array = img_array.T.reshape(WEB_MERCATOR_TILESIZE, WEB_MERCATOR_TILESIZE, reshape).astype('uint8')
+
+        # Create image from array
+        img = Image.fromarray(img_array, mode=mode)
+        stats = {}
 
         # Return rendered image
         return self.write_img_to_response(img, stats)
