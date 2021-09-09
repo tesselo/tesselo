3bf4186b0391c3aefe879d6620c546395580953a
Daniel Moreno <daniel.moreno@tesselo.com>
2021-09-09 11:26:13 +0200

    feat(admin): read only token refresh mechanism

--- apps/raster_api/admin.py
+++ apps/raster_api/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 from guardian.admin import GuardedModelAdmin
 from raster.admin import LegendAdmin, RasterLayerModelAdmin
 from raster.models import Legend, RasterLayer
@@ -33,8 +34,27 @@ class GuardedAggregationLayerModelAdmin(ComputeActivityAggregatesModelAdmin, Gua
     pass
 
 
+class CustomTokenAdmin(TokenAdmin):
+    actions = ['refresh_token']
+
+    def refresh_token(self, request, queryset):
+        """
+        Refresh a read only token by deleting it and creating a new one.
+        """
+        if len(queryset) > 1:
+            messages.error(request, "Please select only one token to refresh")
+            return HttpResponseRedirect(request.get_full_path())
+
+        old_token = queryset[0]
+        user = old_token.user
+        old_token.delete()
+        new_token = ReadOnlyToken.objects.create(user=user)
+        messages.success(request, f"New token created, please copy it: {new_token.key}")
+        return HttpResponseRedirect(request.get_full_path())
+
+
 admin.site.unregister(AggregationLayer)
 admin.site.register(AggregationLayer, GuardedAggregationLayerModelAdmin)
 admin.site.register(PublicAggregationLayer)
 admin.site.register(TesseloUserAccount)
-admin.site.register(ReadOnlyToken, TokenAdmin)
+admin.site.register(ReadOnlyToken, CustomTokenAdmin)
